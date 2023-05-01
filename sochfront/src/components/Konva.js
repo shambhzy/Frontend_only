a1ba20c26b88877bb3cd9d7213e3c1422068b97b
Shambhavi Mallikarjuna <shambz@Shambhavis-MacBook-Pro.local>
2023-05-01 22:33:42 +0530

    Now with konva

---
+++ sochfront/src/components/Konva.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { createRoot } from "react-dom/client";
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
+
+
+
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
+  return (
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      draggable="true"
+      // I will use offset to set origin to the center of the image
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+    />
+  );
+};
+
+const Konva = () => {
+  const dragUrl = React.useRef();
+  const stageRef = React.useRef();
+  const [images, setImages] = React.useState([]);
+  return (
+    <div>
+      Components for Story
+      <br />
+      <img
+        alt="lion"
+        src="https://konvajs.org/assets/lion.png"
+        draggable="true"
+        onDragStart={(e) => {
+          dragUrl.current = e.target.src;
+        }}
+      />
+      <img
+        alt="lion"
+        src="https://konvajs.org/assets/lion.png"
+        draggable="true"
+        onDragStart={(e) => {
+          dragUrl.current = e.target.src;
+        }}
+      />
+      <div
+        onDrop={(e) => {
+          e.preventDefault();
+          // register event position
+          stageRef.current.setPointersPositions(e);
+          // add image
+          setImages(
+            images.concat([
+              {
+                ...stageRef.current.getPointerPosition(),
+                src: dragUrl.current
+              }
+            ])
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={{ border: "2px solid grey" }}
+          ref={stageRef}
+        >
+          <Layer>
+            {images.map((image) => {
+              return <URLImage image={image} />;
+            })}
+          </Layer>
+        </Stage>
+      </div>
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Konva />);
+
+
+
+export default Konva
--- sochfront/src/components/Try.js
+++ sochfront/src/components/Try.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Try.css'
+import myGif from '../images/sb.gif';
+import Konva from './Konva';
 
 function Try() {
   return (
-    <div>Try</div>
+    <div>
+      <h1 className='heading'>
+        Lets make you Your Own Story
+      </h1>
+      <div className='bg-c'></div>
+      <div className='bg-cc'></div>
+
+      {/* Was trying to add cool gifs lol */}
+      {/* <div className='column'>
+        <img  className='gif' src={myGif} alt="imagination-gif"/>
+        <li>
+          <ol>
+            <h3>To start</h3>
+          </ol>
+        </li>
+      </div> */}
+
+      {/* Adding konva canvas */}
+      <Konva/>
+      
+    </div>
   )
 }
 
